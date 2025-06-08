f63d836c5a8c97a420203839ac0af463c47a4683
Filipe <filipe.rocha@aluno.faculdadeimpacta.com.br>
2025-06-08 16:05:51 -0300

    workflow alterado

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,6 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para adicionar o atributo '__version__' em werkzeug
 if not hasattr(werkzeug, '__version__'):
     werkzeug.__version__ = "mock-version"
 
